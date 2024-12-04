4d3816bef68cf026ecdab16c9ca68f87a3f64b1d
Mira Anand <miraanand@comcast.net>
2024-12-03 22:59:19 -0800

    10 minute update - 5

--- lab-37-starter.cpp
+++ lab-37-starter.cpp
@@ -20,6 +20,7 @@ using namespace std;
 // declaration & initialization of global const variables
 const string INPUT_FILE_NAME = "lab-37-data.txt"; // to hold the name of the input file, which contains the string data
 const int MODULUS_VALUE = 97; // represents the value that the sum of the ASCII values will be modulo-ed by in the gen_hash_index() function
+const int MAP_ENTRY_NUM = 100; // represents the # of map entries that should be displayed to the console
 
 // function prototypes
 int gen_hash_index(const string);
@@ -29,10 +30,11 @@ int main()
     // creation of the hash table data structure
     // an std::map, in which the key is an int value that represents the hash index
     // - & the value is an std::list, which will contain the codes from the input file that map to a specific hash index
-    map<int, list<string>> hash_table;
+    map<int, list<string> > hash_table;
     
     // declaration & initialization of variables
     string inputFileLine; // to hold a string value from the input file - will be used to read string data from the input file
+    int mapEntryCounter = 0; // to keep a track of the map entries being displayed to the console
 
     ifstream fin(INPUT_FILE_NAME); // creation of an ifstream (input file) object
 
@@ -45,7 +47,7 @@ int main()
     while (getline(fin, inputFileLine)) // read all of the string values from the input file until the end of the file is reached
     {
         // sending the codes to the gen_hash_index() function as they are being read from the input file
-        // receiving the hash index that is being returned from the function & storing it in a variable
+        // receiving the hash index that is being returned from the function & storing it in a variable named hashIndex
         int hashIndex = gen_hash_index(inputFileLine);
         // inputting the pair into the map
         // using .push_back() to add the string value from the input file to the std::list that corresponds to its generated hash index (which is the key of the map)
@@ -53,6 +55,24 @@ int main()
     }
 
     fin.close(); // close the input file
+
+    // display the first 100 map entries to the console
+    for (auto mapEntry : hash_table) // using a range-based for loop to iterate through the map
+    {
+        cout << "Hash index: " << mapEntry.first << endl; // use .first to access the hash index (key)
+        cout << "Values: ";
+        
+        for (auto value : mapEntry.second) // using another range-based for loop to access/iterate though the std::lists (values/codes) associated with the hash index by using .second
+        {
+            cout << value << " "; // output the string values associated with the hash index/key
+        }
+
+        cout << endl;
+        mapEntryCounter++; // increment the counter, to keep a correct track of the # of map entries being displayed
+
+        if (mapEntryCounter == MAP_ENTRY_NUM) // only display the first 100 (MAP_ENTRY_NUM) map entries and then break from the loop
+            break;
+    }
 
     return 0;
 }

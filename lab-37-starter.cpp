4a6efe33d954f1ab3ccaefb2172703a6101ead1c
Mira Anand <miraanand@comcast.net>
2024-12-03 22:17:31 -0800

    Commit - Back after break

--- lab-37-starter.cpp
+++ lab-37-starter.cpp
@@ -10,9 +10,13 @@ These targets are present in the dataset and can be used for testing:
 E1D2665B21EA
 */
 
+#include <fstream> // needed for file operations
 #include <iostream>
 #include <string>
 using namespace std;
+
+// declaration & initialization of global const variables
+const string INPUT_FILE_NAME = "lab-37-data.txt"; // to hold the name of the input file, which contains the string data
 
 // function prototypes
 int sum_ascii(const string);
@@ -20,12 +24,27 @@ int sum_ascii(const string);
 int main() 
 {
     // declaration & initialization of variables
-    string testString = "My name is Mira"; // to hold the string that we want to test the sum_ascii() function with
+    string inputFileLine; // to hold a string value from the input file - will be used to read string data from the input file
+    int grandTotal = 0; // represents the sum of all ASCII values in the entire file
 
-    // call the sum_ascii() function & apply it to testString
-    // store the result of the function (the sum of the char's ASCII values) in a variable named testResult
-    int testResult = sum_ascii(testString);
-    cout << "The sum of the string's character's ASCII values is equal to " << testResult << endl; // output the results
+    ifstream fin(INPUT_FILE_NAME); // creation of an ifstream (input file) object
+
+    if (!fin) // check if there was an error when opening the input file
+    {
+        cout << "ERROR: Could not open input file. Please make sure the file exists in the correct location & try running the program again." << endl;
+        return 1; // exit with an error state
+    }
+
+    while (getline(fin, inputFileLine)) // read all of the string values from the input file until the end of the file is reached
+    {
+        // call the sum_ascii() function & apply it to inputFileLine
+        // store the result of the function (the sum of the ASCII values) in a variable named grandTotal, which will keep a running total of the sum of all ASCII values in the entire file
+        grandTotal = grandTotal + sum_ascii(inputFileLine);
+    }
+
+    fin.close(); // close the input file
+
+    cout << "The grand total (the sum of all ASCII values in the entire file) is equal to " << testResult << endl; // output the grand total, which is the sum of all ASCII values in the entire file
 
     return 0;
 }

4d946bf66bed5c075e78d561d26fa6d3b4ae988d
Mira Anand <miraanand@comcast.net>
2024-12-03 16:18:06 -0800

    10 minute update - 1

--- lab-37-starter.cpp
+++ lab-37-starter.cpp
@@ -2,21 +2,6 @@
 // Module 15, Lesson: Hash Tables, Assignment: Hash Table I
 // IDE used: Visual Studio Code for Mac
 
-#include <iostream>
-using namespace std;
-
-int main() 
-{
-    char a = 'A';
-    cout << a << endl;
-    cout << (int) a << endl;
-    int b = 66;
-    cout << b << endl;
-    cout << (char) b << endl;
-    
-    return 0;
-}
-
 /* 
 These targets are present in the dataset and can be used for testing:
 536B9DFC93AF
@@ -24,3 +9,29 @@ These targets are present in the dataset and can be used for testing:
 666D109AA22E
 E1D2665B21EA
 */
+
+#include <iostream>
+#include <string>
+using namespace std;
+
+// function prototypes
+int sum_ascii(const string);
+
+int main() 
+{
+
+
+    return 0;
+}
+
+// int sum_ascii(const string stringValue) function header
+// DESCRIPTION:
+// ARGUMENTS:
+// RETURNS:
+int sum_ascii(const string stringValue)
+{
+    int sum = 0; // to hold the sum of the string's character's ASCII values
+
+    for (char charValue : stringValue) // creation of a range-based for loop that will iterate through each of the string's characters
+        sum = sum + (int)
+}
